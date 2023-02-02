5a173fdabb0da30dc0f2f9c2ee18ce954a9e246e
AlanViveros <alan.viverosmo@alumno.buap.mx>
2023-02-01 21:42:20 -0600

    p3

---
+++ practicas/p03/p3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"&gt;
+<html>
+<head>
+    <title>Práctica 2</title>
+</head>
+<body>
+    <h2>Inciso 3</h2>
+    <?php
+    
+    $a = "PHP5";
+    echo "$a<br><br>";
+    $z []="&$a";
+    echo "$z<br><br>";
+    $b = "5a version de php";
+    echo "$b<br><br>";
+    $c = $b*10;
+    echo "$c<br><br>";
+    $a.=$b;
+    echo "$a <br><br>";
+    $b*=$c;
+    echo "$b <br><br>";
+    $z[0]="MYSQL";
+    echo "$z[0]<br<br>";
+
+
+
+
+    
+    ?>
+</body>
+</html>
